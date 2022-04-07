a8a0f3c79fad4dc10c28ba7cc61056f46f8743d6
Valentin-1 <valikv03@gmail.com>
2022-04-07 14:05:19 +0300

    Data trecuta la ore era ploblema cu git, nu am reusit sa fac push

---
+++ MyFirstApp/src/components/showBtn.vue
@@ -0,0 +1,31 @@
+<script>
+export default {
+  components: {},
+  props: {
+    infoList: {
+      type: Object,
+      required: true
+    }
+  },
+  data: () => {
+    return {
+     
+    };
+  },
+  methods: {
+    showInfo(){
+        
+    }
+  }
+};
+</script>
+
+<template>
+  <button class="btn-success" @click="showInfo()">Show</button>
+</template>
+
+<style scoped>
+  button{
+    border-radius: 10px;
+  }
+</style>
---
+++ MyFirstApp/src/views/HomeView.vue
@@ -0,0 +1,52 @@
+<script>
+export default{
+  data(){
+    return{
+      infoList: [
+        {
+          id: 1,
+          nume: "Andrei",
+          phone: "06934324",
+          email: "ion@gmail.com"
+        },
+        {
+          id: 2,
+          nume: "Cristian",
+          phone: "06937543",
+          email: "andr@gmail.com"
+        },
+        {
+          id: 3,
+          nume: "Jhon",
+          phone: "069356564",
+          email: "omar@gmail.com"
+        },
+      ],
+      resultList: [],
+      show: false
+    }
+  },
+  methods: {
+    openInfo(){
+      this.show = !this.show
+      
+    }
+  }
+}
+</script>
+
+<template>
+  <div id="information">
+    <ul>
+      <li v-for="item in infoList" :key="item.id" @click="openInfo()" >
+        {{ item.nume }}
+      </li>
+    </ul>
+    <div v-for="index in infoList" :key="index.nume">
+        <div v-show="this.show" >
+          Phone: {{ index.phone }} <br/>
+          Email: {{ index.email }}
+        </div>
+    </div>
+  </div>
+</template>
